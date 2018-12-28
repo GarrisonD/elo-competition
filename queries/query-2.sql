503af0b5658d51dd200304de6dc2074230763e62
Ihor Dotsenko <igordocenko1997@gmail.com>
2018-12-28 11:00:18 +0000

    TEST LOSS: 3.844

--- queries/query-2.sql
+++ queries/query-2.sql
@@ -2,6 +2,11 @@ COPY(
   SELECT
     card_id,
 
+    COUNT(CASE WHEN purchase_date >= DATE '2018-03-01' - INTERVAL '1 month'  THEN 1 END) as count_transactions_last_1_month,
+    COUNT(CASE WHEN purchase_date >= DATE '2018-03-01' - INTERVAL '3 months' THEN 1 END) as count_transactions_last_3_months,
+    COUNT(CASE WHEN purchase_date >= DATE '2018-03-01' - INTERVAL '6 months' THEN 1 END) as count_transactions_last_6_months,
+    COUNT(CASE WHEN purchase_date >= DATE '2018-03-01' - INTERVAL '1 year'   THEN 1 END) as count_transactions_last_1_year,
+
     SUM(CASE WHEN purchase_date >= DATE '2018-03-01' - INTERVAL '1 month'  THEN ABS(purchase_amount) ELSE 0 END) as sum_purchase_amount_last_1_month,
     SUM(CASE WHEN purchase_date >= DATE '2018-03-01' - INTERVAL '3 months' THEN ABS(purchase_amount) ELSE 0 END) as sum_purchase_amount_last_3_months,
     SUM(CASE WHEN purchase_date >= DATE '2018-03-01' - INTERVAL '6 months' THEN ABS(purchase_amount) ELSE 0 END) as sum_purchase_amount_last_6_months,
