f039204d5d4ccfea80b16cfad0acc35c112c9aff
Ihor Dotsenko <igordocenko1997@gmail.com>
2019-01-26 17:56:02 +0000

    TEST LOSS: 3.811

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-conda install -c conda-forge jupyter scikit-learn pandas feather-format matplotlib seaborn tqdm dask python-graphviz
+conda install -c conda-forge jupyter scikit-learn pandas xlrd matplotlib seaborn tqdm keras
