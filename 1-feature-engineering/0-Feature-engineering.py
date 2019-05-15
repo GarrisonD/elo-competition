8de4c92c3312949f1e6e6a04db3f480dc0a96759
Ihor Dotsenko <igordocenko1997@gmail.com>
2019-05-15 05:24:15 +0000

    Added ipynb versions

--- 1-feature-engineering/0-Feature-engineering.py
+++ 1-feature-engineering/0-Feature-engineering.py
@@ -4,8 +4,8 @@
 #     text_representation:
 #       extension: .py
 #       format_name: light
-#       format_version: '1.3'
-#       jupytext_version: 1.0.3
+#       format_version: '1.4'
+#       jupytext_version: 1.1.1
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
@@ -81,56 +81,6 @@ agg = (
 
 pandas_df = df.groupBy("card_id", "month_lag").agg(*agg).orderBy("card_id", "month_lag").toPandas()
 
-display(pandas_df)
-# -
-
-# Calculate features and save them:
-
-# +
-# %%time
-
 pandas_df.to_feather("../data/1-feature-engineered/aggregated-transactions-by-card-id.feather")
 
-# +
-# %%time
-
-from pyspark.ml.feature import OneHotEncoderEstimator
-from pyspark.ml.stat import Summarizer
-
-df = spark.read.csv("../data/raw/*_transactions.csv", header=True)
-df.printSchema()
-
-# Map authorized flag
-dicts = sc.broadcast(dict([('Y', 1), ('N', 0)]))
-
-def newCols(x): return dicts.value[x]
-callnewColsUdf = F.udf(newCols, T.IntegerType())
-df = df.withColumn("authorized_flag_indexed", callnewColsUdf(F.col("authorized_flag")))
-
-# map installments to int, fix -1 value
-callNewColsUdf = F.udf(lambda x: 999 if x == "-1" else int(x), T.IntegerType())
-df = df.withColumn("installments_fixed", callNewColsUdf(F.col("installments")))
-
-# Produce vector for installments
-# dropLast=True set by default. That produce value 999 to vector of zeros [0]. 
-encoder = OneHotEncoderEstimator(inputCols=["installments_fixed"],
-                                 outputCols=["installments_vec"])
-model = encoder.fit(df)
-df = model.transform(df)
-df.printSchema()
-
-df = df.select("card_id", "month_lag", "purchase_amount", "installments_vec", "authorized_flag_indexed")
-df = df.withColumn("month_lag", F.col("month_lag").cast(T.IntegerType()))
-df = df.withColumn("purchase_amount", F.col("purchase_amount").cast(T.DoubleType()))
-df = df.withColumn("authorized_flag_indexed", F.col("authorized_flag_indexed").cast(T.IntegerType()))
-
-agg = (F.min("purchase_amount"),
-       F.avg("purchase_amount"),
-       F.max("purchase_amount"),
-       F.count("purchase_amount"),
-       F.avg("authorized_flag_indexed"),
-       Summarizer.mean(df.installments_vec))
-
-feature_set = df.groupBy("card_id", "month_lag").agg(*agg).orderBy("card_id", "month_lag")
-
-feature_set.show()
+display(pandas_df)
--- 3-model-engineering/0-Model-engineering.py
+++ 3-model-engineering/0-Model-engineering.py
@@ -4,8 +4,8 @@
 #     text_representation:
 #       extension: .py
 #       format_name: light
-#       format_version: '1.3'
-#       jupytext_version: 1.0.3
+#       format_version: '1.4'
+#       jupytext_version: 1.1.1
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
