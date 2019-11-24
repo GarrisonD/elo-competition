f261b4c0bd1ab2d2c951844fd19597f0afdf1937
Ihor Dotsenko <igordocenko1997@gmail.com>
2019-11-24 19:25:39 +0200

    Upgraded all the project dependencies to the latest versions

--- 0-Base.py
+++ 0-Base.py
@@ -2,11 +2,12 @@
 # jupyter:
 #   jupytext:
 #     cell_metadata_filter: ExecuteTime
+#     formats: ipynb,py
 #     text_representation:
 #       extension: .py
 #       format_name: light
-#       format_version: '1.4'
-#       jupytext_version: 1.2.4
+#       format_version: '1.5'
+#       jupytext_version: 1.3.0
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
